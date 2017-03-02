f391e3c84e8429e69ebc48aefd8b5f34080e9275
Ted Callahan <CCallahanIV@gmail.com>
2017-03-02 15:05:23 -0800

    Added applications to installed_apps in settings.py

--- pylistener/pylistener/settings.py
+++ pylistener/pylistener/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pylistener_profile',
+    'pylistener_addressbook',
+    'pylistener_catatt'
 ]
 
 MIDDLEWARE = [
