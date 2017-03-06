fe7acb3adf15394d15ee53902a603b855031544a
Ted Callahan <CCallahanIV@gmail.com>
2017-03-06 10:49:43 -0800

    Added base and home templates, wrote initial setup of profile model and profile views.

--- pylistener/pylistener_profile/views.py
+++ pylistener/pylistener_profile/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+
+class ManageView(LoginRequiredMixin, TemplateView):
+    """View for user's management page."""
+
+    template_name = "imager_images/library.html"
+    login_url = reverse_lazy("login")
+
+    def get_context_data(self):
+        """Get required context data for manage view."""
+
+
+        return {"addresses": addresses, "categories": categories, "attributes": attribtues}
